3952145fbbbf7540c308b11dff985672ee49bc5e
Jaber Shaikh <ShaikhJ@regents.ac.uk>
2023-02-12 22:57:29 +0530

    Wagon data

--- src/main/java/com/cricket/model/Wagon.java
+++ src/main/java/com/cricket/model/Wagon.java
@@ -12,12 +12,9 @@ public class Wagon {
   @XmlElement(name = "wagonNumber")
   private int wagonNumber;
 
-  @XmlElement(name = "wagonXCoOrd")
-  private String wagonXCoOrd;
+  @XmlElement(name = "wagonData")
+  private String wagonData;
 
-  @XmlElement(name = "wagonYCoOrd")
-  private String wagonYCoOrd;
-  
   @XmlElement(name = "batterId")
   private int batterId;
 
@@ -40,12 +37,11 @@ public Wagon() {
 	super();
 }
 
-public Wagon(int wagonNumber, String wagonXCoOrd, String wagonYCoOrd, int batterId, int bowlerId, int runs,
-		int inningNumber, int overNumber, int ballNumber) {
+public Wagon(int wagonNumber, String wagonData, int batterId, int bowlerId, int runs, int inningNumber, int overNumber,
+		int ballNumber) {
 	super();
 	this.wagonNumber = wagonNumber;
-	this.wagonXCoOrd = wagonXCoOrd;
-	this.wagonYCoOrd = wagonYCoOrd;
+	this.wagonData = wagonData;
 	this.batterId = batterId;
 	this.bowlerId = bowlerId;
 	this.runs = runs;
@@ -54,28 +50,20 @@ public Wagon(int wagonNumber, String wagonXCoOrd, String wagonYCoOrd, int batter
 	this.ballNumber = ballNumber;
 }
 
+public String getWagonData() {
+	return wagonData;
+}
+
+public void setWagonData(String wagonData) {
+	this.wagonData = wagonData;
+}
+
 public int getWagonNumber() {
 	return wagonNumber;
 }
 
 public void setWagonNumber(int wagonNumber) {
 	this.wagonNumber = wagonNumber;
-}
-
-public String getWagonXCoOrd() {
-	return wagonXCoOrd;
-}
-
-public void setWagonXCoOrd(String wagonXCoOrd) {
-	this.wagonXCoOrd = wagonXCoOrd;
-}
-
-public String getWagonYCoOrd() {
-	return wagonYCoOrd;
-}
-
-public void setWagonYCoOrd(String wagonYCoOrd) {
-	this.wagonYCoOrd = wagonYCoOrd;
 }
 
 public int getBatterId() {
