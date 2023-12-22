ee1a72167ae87b76223dac1c2e768cf267579d66
Doad Software Department <98625971+DoadSoftware@users.noreply.github.com>
2023-12-22 16:26:18 +0530

    UPDATE ICC
    added text1 to text3

--- src/main/java/com/cricket/model/Player.java
+++ src/main/java/com/cricket/model/Player.java
@@ -74,7 +74,18 @@ public class Player implements Comparable<Player>
   @Column(name = "Nationality")
   private String Nationality;
   
-  @Transient
+  @Column(name = "Text1")
+  private String text1;
+  
+  @Column(name = "Text2")
+  private String text2;
+  
+  @Column(name = "Text3")
+  private String text3;
+  
+
+
+@Transient
   private Integer playerPosition;
 
   @Transient
@@ -269,7 +280,6 @@ public void setAge(Integer age) {
 	this.age = age;
 }
 
-
 public String getTwitterHandle() {
 	return twitterHandle;
 }
@@ -284,6 +294,30 @@ public String getInstagramHandle() {
 
 public void setInstagramHandle(String instagramHandle) {
 	this.instagramHandle = instagramHandle;
+}
+
+public String getText1() {
+	return text1;
+}
+
+public void setText1(String text1) {
+	this.text1 = text1;
+}
+
+public String getText2() {
+	return text2;
+}
+
+public void setText2(String text2) {
+	this.text2 = text2;
+}
+
+public String getText3() {
+	return text3;
+}
+
+public void setText3(String text3) {
+	this.text3 = text3;
 }
 
 @Override
