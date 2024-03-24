9d5ab3709545efd08b8b0ec4f082a450fe86ea59
Doad Software Department <98625971+DoadSoftware@users.noreply.github.com>
2024-03-24 15:44:18 +0530

    UPDATE

--- src/main/java/com/cricket/util/CricketFunctions.java
+++ src/main/java/com/cricket/util/CricketFunctions.java
@@ -6871,7 +6871,7 @@ public class CricketFunctions {
 					  switch (events.get(i).getEventType().toUpperCase()) {
 					    case CricketUtil.ONE : case CricketUtil.TWO: case CricketUtil.THREE:  case CricketUtil.FIVE : case CricketUtil.DOT: case CricketUtil.FOUR: 
 					    case CricketUtil.SIX: case CricketUtil.WIDE: case CricketUtil.NO_BALL: case CricketUtil.BYE: case CricketUtil.LEG_BYE: case CricketUtil.PENALTY:
-					    case CricketUtil.LOG_WICKET: case CricketUtil.LOG_ANY_BALL:
+					    case CricketUtil.LOG_WICKET: case CricketUtil.LOG_ANY_BALL: case CricketUtil.NINE:
 					    	
 					    	total_runs = total_runs + events.get(i).getEventRuns();
 					    	
