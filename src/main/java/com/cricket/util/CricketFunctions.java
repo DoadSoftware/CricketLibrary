a09e93a0cefe3069c599e64b387fbe3af9bcabbe
Jaber Shaikh <Jaber.Shaikh@regents.ac.uk>
2025-08-02 15:03:30 +0530

    targetData fix

--- src/main/java/com/cricket/util/CricketFunctions.java
+++ src/main/java/com/cricket/util/CricketFunctions.java
@@ -16221,8 +16221,8 @@ public class CricketFunctions {
 	        	targetData.setTargetRuns(GetTeamRunsAhead(1, matchAllData) + 1);
 	        }
 	    }
-	
-	    if (matchAllData.getSetup().getTargetOvers() != null && Integer.valueOf(matchAllData.getSetup().getTargetOvers()) > 0) {
+	    if (matchAllData.getSetup().getTargetOvers() != null && !matchAllData.getSetup().getTargetOvers().isEmpty() 
+	    		&& Integer.valueOf(matchAllData.getSetup().getTargetOvers()) > 0) {
 	    	targetData.setTargetOvers(matchAllData.getSetup().getTargetOvers());
 	    } else {
 	    	targetData.setTargetOvers(String.valueOf(matchAllData.getSetup().getMaxOvers()));
